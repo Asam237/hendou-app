08a61afc3d2daa1a960b26c2a3ecaa18cb95f85e
asam237 <abbasalimokolo@gmail.com>
2023-01-17 16:26:40 +0100

    fix: mongoose version and check the path

--- api/src/core/config/index.ts
+++ api/src/core/config/index.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv"
 
 config()
 
-export const MONGO_URI = process.env.MONG_URI
+export const MONGO_URI = process.env.MONGO_URI
 export const PORT = process.env.PORT
 export const JWT_SECRET = process.env.JWT_SECRET
 export const EXPIRES = process.env.EXPIRES
