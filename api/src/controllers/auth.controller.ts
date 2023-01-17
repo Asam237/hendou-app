67c55ac933d50be9e8126169e176ae1e779555fa
asam237 <abbasalimokolo@gmail.com>
2023-01-17 16:00:23 +0100

    feat: auth login

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response, NextFunction } from "express"
 import * as bcrypt from "bcryptjs"
 import { UserModel } from "../models/user.model"
+import { TokenInfo } from "../core/types"
+import * as jwt from "jsonwebtoken"
+import { EXPIRES, JWT_SECRET } from "../core/config"
+
 class AuthController {
     public static async create(req: Request, res: Response, next: NextFunction): Promise<any> {
         try {
@@ -10,6 +14,28 @@ class AuthController {
             const user: any = new UserModel(userParam)
             await user.save()
             res.status(200).json({ user })
+
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+    public static async login(req: Request, res: Response, next: NextFunction): Promise<any> {
+        try {
+            const { email, password }: any = req.body
+            const user: any = await UserModel.findOne({ email })
+            if (!user) {
+                res.status(403).json({ message: "login failed" })
+            }
+            const isMatch: boolean = bcrypt.compareSync(password, user.password)
+            if (!isMatch) {
+                res.status(403).json({ message: "login failed" })
+            }
+            const { _id } = user
+            const tokenInfo: TokenInfo = {
+                id: _id
+            }
+            const token = jwt.sign(tokenInfo, JWT_SECRET, { expiresIn: EXPIRES })
+            res.status(200).json({ token })
 
         } catch (error) {
             res.status(500).json(error)
